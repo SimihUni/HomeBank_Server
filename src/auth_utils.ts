a77f4524a3f86b913985e729578f8cd3dc862314
SimihUni <simih_school@abv.bg>
2024-01-04 09:47:34 +0200

    print typeof

--- src/auth_utils.ts
+++ src/auth_utils.ts
@@ -28,6 +28,7 @@ export async function authenticateBearer (
 
 export async function generateAuthTokens (email: string, isAdmin: boolean, prJWK: jose.JWK = privateJWK) {
   try {
+    console.log(typeof(privateJWK))
     const accessToken = await new jose.SignJWT({ isAdmin })
       .setIssuer(host)
       .setIssuedAt()
